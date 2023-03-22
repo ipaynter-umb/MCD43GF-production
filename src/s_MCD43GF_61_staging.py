f62117cc057f9da8d1f28fc85c0b19c8430912b4
ipaynter-umb <ian.paynter@umb.edu>
2023-03-22 10:08:05 -0400

    Overhaul integrating new LAADS tools

--- src/s_MCD43GF_61_staging.py
+++ src/s_MCD43GF_61_staging.py
@@ -1,5 +1,23 @@
 import argparse
 import t_mcd43gf
+import logging
+from os import environ
+
+# Set the logging config
+logging.basicConfig(filename=environ['log_files_path'] + f'{datetime.datetime.now():%Y%m%d%H%M%S}.log',
+                    filemode='w',
+                    format=' %(levelname)s - %(asctime)s - %(message)s',
+                    level=logging.INFO)
+# Band list
+band_list = [31, 40]
+# List of bands from 1 - 31
+band_list += list(arange(1, 31, 1))
+# For band in bands
+for band in band_list:
+    # Log info
+    logging.info(f"Processing band {band}.")
+    # Get the input data for the band
+    get_input_data_for_band(years, band, archive_set=archive_set)
 
 if __name__ == '__main__':
 
